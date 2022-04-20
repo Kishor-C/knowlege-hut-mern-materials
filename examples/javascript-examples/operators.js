b91babc2d1483352bc833483f967f9ce975cbbb5
Kishor <kishor1124@gmail.com>
2022-04-20 19:50:39 +0530

    js examples

---
+++ examples/javascript-examples/operators.js
@@ -0,0 +1,16 @@
+var a = 5;
+var b = 2;
+var c = a * b;
+document.write("<div>c = "+c+"</div>");
+var d = a > b;
+document.write("<div>d = "+d+"</div>");
+var e = a++;
+document.write("<div>a = "+a+"</div>");
+document.write("<div>e = "+e+"</div>");
+var num1 = "20";
+var num2 = 20;
+var isNumber1 = (num1 == num2);
+var isNumber2 = (num1 === num2);
+document.write("<div>isNumber1: "+isNumber1+"</div>");
+document.write("<div>isNumber2: "+isNumber2+"</div>");
+
