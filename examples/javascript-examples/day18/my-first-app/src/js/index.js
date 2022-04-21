6b4f75bac53a92f034b9fa1e2bdcd49322b8d476
Kishor <kishor1124@gmail.com>
2022-04-21 20:01:13 +0530

    javascript example

--- examples/javascript-examples/day18/my-first-app/src/js/index.js
+++ examples/javascript-examples/day18/my-first-app/src/js/index.js
@@ -1,13 +1,13 @@
+var a = prompt("Enter value of a: ");
+var b = prompt("Enter value of b: ");
+a = parseInt(a);
+b = parseInt(b);
+var c = a + b;
 var ele = document.getElementById('main');
-console.log(ele);
-console.log('------------------');
-// innerHTML is a property of an Element that accesses content inside the element
-console.log(ele.innerHTML);
-ele.innerHTML = '<h3>Hello Javascript</h3>'; // this changes content of div: main
-ele.style.color = 'red'; // <div id = 'main' style = 'color:red'>
-ele.style.fontSize = '30px';
-
-var eleStyle = ele.style;
-eleStyle.color = 'green'; // ele.style.color = 'green';
-eleStyle.fontSize = '12px';
-eleStyle.backgroundColor = 'yellow';
+if (a > b) {
+    ele.innerHTML = 'a is > b and value of c = '+c;
+    ele.style.color = 'green';
+} else {
+    ele.innerHTML = 'a is not > b and value of c = '+c;
+    ele.style.color = 'red';
+}
