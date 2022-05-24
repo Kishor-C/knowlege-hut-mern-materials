c13a4ccfaa0313daeedf69d2b635957d9ece27e7
Kishor <kishor1124@gmail.com>
2022-05-24 19:23:43 +0530

    conditional and controlled components

---
+++ examples/react-examples/day38,39/my-second-app/src/App.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { Success } from './components/Authenticate';
+import { Forms } from './components/Forms';
+
+function App() {
+ return (<div className = 'container-fluid'>
+   <Forms />
+ </div>)
+}
+
+export default App;
